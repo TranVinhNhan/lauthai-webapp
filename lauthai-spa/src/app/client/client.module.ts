88de44899eabff3a2a2bd0a0baf58563e098f4e8
hung.doan <hung.doan@HCM-HUNGDM>
2020-09-03 16:43:40 +0700

    new hung

--- lauthai-spa/src/app/client/client.module.ts
+++ lauthai-spa/src/app/client/client.module.ts
@@ -4,21 +4,32 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {MatTableModule} from '@angular/material/table';
+
+
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { BannerComponent } from './banner/banner.component';
+import { TableComponent } from './table/table.component';
+import { DataTbl2Component } from './data-tbl2/data-tbl2.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  declarations: [ClientComponent, NavbarComponent],
+  declarations: [ClientComponent, NavbarComponent, BannerComponent, TableComponent, DataTbl2Component],
   imports: [
     CommonModule,
     ClientRoutingModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class ClientModule { }
