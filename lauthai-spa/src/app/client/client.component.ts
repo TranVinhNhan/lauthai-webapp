2328905b1edffb6b5fa50c74318b7591ac87adf5
nhanvinh <tranvinhnhan.tech@gmail.com>
2020-09-03 13:18:54 +0700

    migrate to scss & navbar 1/2

--- lauthai-spa/src/app/client/client.component.ts
+++ lauthai-spa/src/app/client/client.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.sass']
+  styleUrls: ['./client.component.scss']
 })
 export class ClientComponent implements OnInit {
-
+  showFiller = false;
   constructor() { }
 
   ngOnInit(): void {
--- lauthai-spa/src/app/client/client.module.ts
+++ lauthai-spa/src/app/client/client.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [ClientComponent],
+  declarations: [ClientComponent, NavbarComponent],
   imports: [
     CommonModule,
-    ClientRoutingModule
+    ClientRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class ClientModule { }
