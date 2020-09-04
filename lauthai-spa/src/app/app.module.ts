172122de73e00f34d9ce11a41f175e3bd841fab0
nhanvinh <tranvinhnhan.tech@gmail.com>
2020-09-04 09:24:15 +0700

    fix wrong component

--- lauthai-spa/src/app/app.module.ts
+++ lauthai-spa/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataTableComponent } from './data-table/data-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataTableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
