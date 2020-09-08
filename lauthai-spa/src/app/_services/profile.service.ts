89a765d39d7bbc829d97366ad98bf806599af43e
nhanvinh <tranvinhnhan.tech@gmail.com>
2020-09-08 10:41:48 +0700

    fixed navbar link & added images to service

--- lauthai-spa/src/app/_services/profile.service.ts
+++ lauthai-spa/src/app/_services/profile.service.ts
@@ -24,6 +24,21 @@ const DISTRICT: string[] = [
   'Quận Thủ Đức', 'Quận Gò Vấp'
 ];
 
+const PFPURL: string[] = [
+  'https://danongonline.com.vn/wp-content/uploads/2018/02/anh-girl-xinh-9-1.jpg',
+  'https://2.bp.blogspot.com/-fjf5yU5r1Jk/WE1VD1BBKpI/AAAAAAAAjgI/bXwGoigAPJYvScMPtzJtzbOJfoGQO2C_ACEw/s1600/15349541_533868826819201_3350340522319981193_n.jpg',
+  'https://phunugioi.com/wp-content/uploads/2020/04/anh-gai-xinh-2000-de-thuong.jpg',
+  'https://4.bp.blogspot.com/-983QtP5mKzY/WyEd9tw7lYI/AAAAAAAAwPA/hHxSPxoXZUMbfn8bYZbFhJzsRUrFOzsygCLcBGAs/s640/anh-girl-xinh-chon-loc-1.jpg',
+  'https://sohanews.sohacdn.com/2020/2/26/photo-1-158270587240769675748.jpg',
+  'https://thuthuatnhanh.com/wp-content/uploads/2019/12/hinh-anh-gai-xinh-de-thuong-2.jpg',
+  'https://danongonline.com.vn/wp-content/uploads/2018/02/anh-girl-xinh-ha-noi-6.jpg',
+  'https://cdn.24h.com.vn/upload/2-2019/images/2019-04-19/1555668648-429-ppppppppppp-1555668639-width650height813.jpg',
+  'https://congngheads.com/media/images/anh-dep/hinh-nen-dep-2016-1559616366/bo-anh-gai-xinh-ao-do-dang-la-luot-tren-phim-dan-lam-hinh-nen-20-07-2019-1.jpg',
+  'https://i.pinimg.com/236x/0d/7d/b0/0d7db08b7a4a5e31c2f8bfb353b0a882.jpg',
+  'https://i.pinimg.com/564x/3c/dd/56/3cdd56556aaf558988e225c312d34e97.jpg',
+  'https://congngheads.com/media/images/anh-dep/hinh-nen-android-dep-1559622870/anh-gai-xinh-mac-kimono-hong-duyen-dang-04-08-2019-2.jpg'
+]
+
 @Injectable({
   providedIn: 'root'
 })
@@ -45,10 +60,12 @@ export class ProfileService {
 
     const dist = DISTRICT[Math.round(Math.random() * (DISTRICT.length - 1))];
 
+    const url = PFPURL[Math.round(Math.random() * (PFPURL.length - 1))];
+
     return {
       id,
       name,
-      pfpUrl: null,
+      pfpUrl: url,
       university: uni,
       age,
       job: 'Sinh viên',
